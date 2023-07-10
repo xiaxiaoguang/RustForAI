f8de4c228251cfcfe5e762a187aff5cf19ebb030
xiaxiaoguang <2645982938@qq.com>
2023-07-10 15:54:57 +0800

    try

--- src/Loss/mod.rs
+++ src/Loss/mod.rs
@@ -4,3 +4,32 @@ use ndarray::{self, Array2};
 pub struct CrossEntrophyLoss{
     ot_x : Box<Array2<f32>>,
 }
+
+fn gmax(a : f32, b : f32)->f32{
+    if a>b {return a;}
+    return b;
+}
+
+fn max(x : &Array2<f32>)->f32{
+    let mut a = 0.0;
+    for i in x.iter(){
+        a = gmax(a,*i);
+    }
+    a
+}
+
+impl CrossEntrophyLoss{
+    fn new(qwq : &Array2<f32>)->CrossEntrophyLoss{
+        let mut x = qwq.clone();
+        x -= max(&x);
+        x  = x.map(|x|x.exp());
+        x /= x.sum();
+        Self{
+            ot_x : Box::new(x),
+        }
+    }
+    fn backward(&self,st_x : &Array2<f32>)->Array2<f32>{
+        (&(*self.ot_x) - st_x).clone()
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,8 @@ mod layer;
 mod dataset;
 mod module;
 mod optimizer;
-mod Loss;
-
-use rand::distributions::{Distribution, Uniform};
-
+mod loss;
 fn main() {
-    let between = Uniform::<f32>::new(0.0,1.0);
-    let mut rng = rand::thread_rng();
-    let mut sum = 0.0;
-    for _ in 0..1000 {
-        sum += between.sample(&mut rng);
-    }
-    println!("{}", sum);
+    println!("qwq world!");
+    return ;
 }
